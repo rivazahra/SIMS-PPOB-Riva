fb2db2fcf73088c574b2c906cfe5cf40c8ea727e
rivazahra <riva.zahra88@gmail.com>
2026-01-12 12:08:37 +0700

    Add Assets

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import Wallet from './walletlogo.png'
+import AuthImg from './AuthImg.png'
+import Avatar from './Avatar.png'
+import BgSaldo from'./bgsaldo.png'
+export {Wallet,AuthImg, Avatar, BgSaldo}
